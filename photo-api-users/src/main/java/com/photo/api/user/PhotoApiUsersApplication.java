a5fcf6c39f3dc3f7ae81e5d6882c519e3207da87
Sachith Gunasekara <wadsachi@gmail.com>
2021-10-26 13:18:16 +0530

    Added histrix fall tolerance

--- photo-api-users/src/main/java/com/photo/api/user/PhotoApiUsersApplication.java
+++ photo-api-users/src/main/java/com/photo/api/user/PhotoApiUsersApplication.java
@@ -2,6 +2,7 @@ package com.photo.api.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -15,6 +16,7 @@ import feign.Logger.Level;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class PhotoApiUsersApplication {
 
 	public static void main(String[] args) {
--- photo-api-users/src/main/java/com/photo/api/user/ui/service/client/album/AlbumServiceClient.java
+++ photo-api-users/src/main/java/com/photo/api/user/ui/service/client/album/AlbumServiceClient.java
@@ -1,16 +1,49 @@
 package com.photo.api.user.ui.service.client.album;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.photo.api.user.ui.model.dto.album.AlbumResponseModel;
 
-@FeignClient("album-ws")
+import feign.hystrix.FallbackFactory;
+
+@FeignClient(name = "album-ws", fallbackFactory = AlbumFallbackFactory.class)
 public interface AlbumServiceClient {
 
-	@GetMapping(path = "/users/{id}/albumss")
-	public List<AlbumResponseModel> getAlbums(@PathVariable(name = "id",required = true) String userId);
+	@GetMapping(path = "/users/{id}/albums")
+	public List<AlbumResponseModel> getAlbums(@PathVariable(name = "id", required = true) String userId);
+}
+
+@Component
+class AlbumFallbackFactory implements FallbackFactory<AlbumServiceClient> {
+
+	@Override
+	public AlbumServiceClient create(Throwable cause) {
+		return new AlbumServiceClientFallback(cause);
+	}
+}
+
+class AlbumServiceClientFallback implements AlbumServiceClient {
+
+	private Throwable cause;
+
+	
+	public AlbumServiceClientFallback() {
+	}
+
+	public AlbumServiceClientFallback(Throwable cause) {
+		this.cause = cause;
+	}
+
+	@Override
+	public List<AlbumResponseModel> getAlbums(String userId) {
+		System.out.println(cause.getLocalizedMessage());
+		return new ArrayList<>();
+	}
+
 }
